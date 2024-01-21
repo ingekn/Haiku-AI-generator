a9b13b941b442c8379b48e239b23229b62948eaf
ingekn <133350400+ingekn@users.noreply.github.com>
2024-01-21 22:47:34 +0000

    Added box shadow

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ function handleResponse(response) {
 
 function handleSubmit(event) {
   event.preventDefault();
+  let apiKey = "1386aafaa966aa68e4520o87btc31531";
   let context =
     "You are a wonderful japanese haiku poem generator full of inspiration and happy to assist. You always generate beautiful haiku's every time when asked about the provided theme and please when generating the haiku you do this in HTML format. Example format: <p>Green and small he sits,</p><p>In the pond, his home so calm,</p> <p>Toad croaks through the night.</p> please do not deviate from this format. In case you do not understand the theme or incorrect theme was provided or no theme at all, please create a haiku about choosing a theme, but always generate it provided in the HTML as seen in the example";
   let prompt = document.querySelector("#theme-input").value;
@@ -17,6 +18,5 @@ function handleSubmit(event) {
   axios.get(apiUrl).then(handleResponse);
 }
 
-let apiKey = "1386aafaa966aa68e4520o87btc31531";
 let formSubmitElement = document.querySelector("#form-submit");
 formSubmitElement.addEventListener("submit", handleSubmit);
